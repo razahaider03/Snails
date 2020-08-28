2246cdb0fd725f8e904239ccc0897349cde73468
Raza Haider <razahaider03@gmail.com>
2020-08-28 21:49:11 +0530

    Now fill the corral added

---
+++ app/src/main/java/HerdManager/FillTheCorral.java
@@ -0,0 +1,13 @@
+package HerdManager;
+
+import java.util.Random;
+
+/**
+ * This class uses your Gate class to fill the corral with snails. We have supplied you will the
+ * code necessary to execute as an app. You must fill in the missing logic below.
+ */
+public class FillTheCorral extends Gate {
+
+  // TODO -- Fill your code in here
+
+}
