1e7c09cb0f108454d97f47a782987913cd87d4da
Raza Haider <razahaider03@gmail.com>
2020-08-29 15:02:24 +0530

    Fill the Corral

--- app/src/main/java/HerdManager/FillTheCorral.java
+++ app/src/main/java/HerdManager/FillTheCorral.java
@@ -8,69 +8,67 @@ import java.util.Random;
  */
 public class FillTheCorral extends Gate {
 
-    /**
-     * Select a predictable seed for the RandomNumber generator.
-     */
-    public final static int sRANDOM_SEED = 1234;
+  /** Select a predictable seed for the RandomNumber generator. */
+  public static final int sRANDOM_SEED = 1234;
 
-    /**
-     * The maximum number of Gate objects.
-     */
-    private final static int sMAX_GATES = 4;
+  /** The maximum number of Gate objects. */
+  private static final int sMAX_GATES = 4;
 
+  // TODO -- Fill your code in here
 
-    // TODO -- Fill your code in here
+  public static void main(String[] args) {
+    final Random randomNumber = new Random(sRANDOM_SEED);
+    final FillTheCorral mFillTheCorral = new FillTheCorral();
+    final Gate[] corral = new Gate[sMAX_GATES];
+    for (int i = 0; i < corral.length; i++) corral[i] = new Gate();
 
-    public static
-    void main ( String[] args ) {
-        final Random randomNumber =
-                new Random(sRANDOM_SEED);
-        final FillTheCorral mFillTheCorral =
-                new FillTheCorral();
-        final Gate[] corral =
-                new Gate[sMAX_GATES];
-        for (int i = 0; i < corral.length; i++)
-            corral[i] = new Gate();
+    do {
+      // Randomly set the direction of each gate's swing in
+      // the corral array.
+      mFillTheCorral.setCorralGates(corral, randomNumber);
+    } while (!mFillTheCorral.anyCorralAvailable(corral));
 
-        do {
-            // Randomly set the direction of each gate's swing in
-            // the corral array.
-            mFillTheCorral.setCorralGates(corral,
-                    randomNumber);
-        } while (!mFillTheCorral.anyCorralAvailable(corral));
+    // Corral all the snails.
+    mFillTheCorral.corralSnails(corral, randomNumber);
+  }
 
-        // Corral all the snails.
-        mFillTheCorral.corralSnails(corral, randomNumber);
+  // sets random directions for each gate
+  public void setCorralGates(Gate[] gate, Random selectDirection) {
+    for (Gate ignored : gate) {
+      setSwing(selectDirection.nextInt(3) - 1);
     }
+  }
 
-    public void setCorralGates(Gate[] gate, Random selectDirection) {
-        for (Gate ignored : gate) {
-
-            Gate.setSwing(selectDirection.nextInt(1) - 1);
-        }
+  // checks if there is at least one IN open gate
+  public boolean anyCorralAvailable(Gate[] corral) {
+    for (Gate ignored : corral) {
+      if (getSwingDirection() == IN) {
+        return true;
+      }
     }
-    public boolean anyCorralAvailable(Gate[] corral) {
-        for ( Gate ignored :corral) {
+    return false;
+  }
 
-            if (Gate.getSwingDirection() == Gate.IN) return true;
-        }
-        return false;
+  // methods finds number of iterations to corral all snails
+  public int corralSnails(Gate[] corral, Random rand) {
+    // variable to track number of snails
+    int totalNumSnails = 5;
+    // variable to track number of iterations
+    int counter = 0;
+    // loop for iterations which terminates where all snails are in a corral
+    while (totalNumSnails > 0) {
+      // picks a random corral
+
+      // picks a random number of snails from the pasture
+      int currentCorral = rand.nextInt(corral.length);
+      int numSnails = rand.nextInt(totalNumSnails) + 1;
+      // updates the total number of snails
+      totalNumSnails = totalNumSnails - corral[currentCorral].thru(numSnails);
+      // prints out the state
+      System.out.print(numSnails + " are trying to move through corral " + currentCorral + "\n");
+      // increments the counter
+      counter++;
     }
-    public void corralSnails(Gate [] corral, Random rand) {
-        int snails = 5;
-        System.out.println("Initial gate setup");
-        int trying = 0;
-        do {
-            int randNum =rand.nextInt(corral.length)+1;
-            int s = rand.nextInt(snails)+1;
-            Gate gate = corral[randNum];
-            snails -= gate.thru(s);
-            System.out.println(snails+ "are trying to move through corral "+ randNum);
-            trying ++;
-        }while (snails != 0);
-        System.out.println("It took "+ trying+ " attempts to corral all of the snails");
-
-    }
-
+    return counter;
+  }
 }
-
